768908877ef8c82e3e5a56d160995a3818388b01
Ahmed Bani-Salameh <85875160+ahmedbani@users.noreply.github.com>
2022-03-27 18:56:33 +0300

    Update publish.sh

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -2,6 +2,6 @@
 
 
 pwd
-scp ./jenkins/deploy/deploy.sh ahmed@34.125.84.234
-yes | ssh -i ../../.ssh/id_rsa ahmed@application-vm "./deploy.sh"
+scp ./jenkins/deploy/deploy.sh ahmed@34.125.84.234:
+ssh ahmed@application-vm "./deploy.sh"
 
