9179814b3aa88891ed1ce38fd503cb348beafe73
Ahmed Bani-Salameh <85875160+ahmedbani@users.noreply.github.com>
2022-03-29 18:04:00 +0300

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -7,7 +7,7 @@ echo "tagging image"
 
 docker tag robofriends gcr.io/planar-root-344213/robofriends
 
-docker login -u oauth2accesstoken -p "ya29.A0ARrdaM8nEsJoKnmx77ywRsg3jx2jY5n_x0UPZVPJ7hTyTEzSqzK280RBeT4kpGjnBBmFu5LqKOu9TqWuz0A0Vs0ihp3zzBB87iYv96PScO1Ix4H0_P_FdPHlaHEWkGnMgrDd9Y4UygXq7XZBsku27OmhATFrEcl22Y3y1xve3KV60ya9BQdWu5S0mZfdyzh1k2XXFDFcGWBGQPBZwalSCh9nfKCPLRvRnEqdsdzOPKSeOE-sgb8TCPcDGVCsjz6p1d-9vF0" https://gcr.io
+docker login -u oauth2accesstoken -p "ya29.A0ARrdaM-pTuUQ-VHaVx0ABC5IGYcu72Ykzt1JyGMmKbT6qkrwAMK4dr9OgFvCuOUPDzS9LGhYSCFy1wenNN9CR-w1qcwe9nR4eDHByWuac7wHzOK_UYnCElBEjVMOEx1Nn41oE7Vvr3Me5zGrtXTpXNOCXzde1QmixyeMiDob_wfLfbvggCm6N9nJf6F9869azZN-48mA9_9N2hLRlf8CG0IY34am7_nHE_Y2zpEsHAjKd1dMiYhXDsQq_T_x891y8ovnq1w" https://gcr.io
 
 echo "pushing the image"
 
