808f5a7eca6b9454a37561fba60b64fd34d64d66
Ahmed Bani-Salameh <85875160+ahmedbani@users.noreply.github.com>
2022-03-29 18:09:12 +0300

    Update publish.sh

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -3,6 +3,6 @@
 
 pwd
 ls
-scp jenkins/deploy/deploy.sh ahmed@34.125.84.234:
-ssh ahmed@34.125.84.234 "./deploy.sh"
+# scp jenkins/deploy/deploy.sh ahmed@34.125.84.234:
+ssh ahmed@34.125.84.234 "docker run -p 3000:3000 gcr.io/planar-root-344213/robofriends -t robofriends"
 
